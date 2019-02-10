b5b530a40a8089155dc710267786ef3b602caca9
Роман Шаповал <romansapoval@Romans-MacBook-Air.local>
2019-02-10 18:08:10 +0200

    common components structure init

---
+++ app/src/components/common/button/Button.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Button.scss';
+
+export class Button extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
---
+++ app/src/components/common/input/InputField.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './InputField.scss';
+
+export class InputField extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
