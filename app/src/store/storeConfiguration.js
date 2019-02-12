6d66e201b6f01f91b02e5aa2dbe1c59ac432e77c
Роман Шаповал <romansapoval@Romans-Air.lan>
2019-02-13 00:00:26 +0200

    loger midleware deleted

--- app/src/store/storeConfiguration.js
+++ app/src/store/storeConfiguration.js
@@ -3,18 +3,8 @@ import {APP_STATE_DEFAULT} from "../consts/AppStateDefault";
 import thunk from "redux-thunk";
 import {rootReducer} from "./rootReducer";
 
-const logger = store => next => action => {
-    let result;
-    console.groupCollapsed("dispatching", action.type);
-    console.log('prev state', store.getState());
-    console.log('action', action);
-    result = next(action);
-    console.log('next state', store.getState());
-    console.groupEnd();
-};
-
 const storePersons = (initialState=APP_STATE_DEFAULT) =>
-    applyMiddleware(logger, thunk)(createStore)(
+    applyMiddleware(thunk)(createStore)(
     rootReducer,
     initialState
 );
